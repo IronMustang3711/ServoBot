ed029f5a067ec1fda8cfd1fbaeaf5d3ed993b41e
jasonnn <jasonmarkham@gmail.com>
2017-02-02 15:46:36 -0800

    remove unused code

--- ServoBot/src/Robot.cpp
+++ ServoBot/src/Robot.cpp
@@ -9,37 +9,16 @@
 #include <RobotDrive.h>
 #include <Timer.h>
 
-/**
- * This is a demo program showing the use of the RobotDrive class.
- * The SampleRobot class is the base of a robot application that will
- * automatically call your Autonomous and OperatorControl methods at the right
- * time as controlled by the switches on the driver station or the field
- * controls.
- *
- * WARNING: While it may look like a good choice to use for your code if you're
- * inexperienced, don't. Unless you know what you are doing, complex code will
- * be much more difficult under this system. Use IterativeRobot or Command-Based
- * instead if you're new.
- */
-class Robot: public frc::SampleRobot {
 
-	frc::Joystick stick { 0 }; // only joystick
-	frc::SendableChooser<std::string> chooser;
-	const std::string autoNameDefault = "Default";
-	const std::string autoNameCustom = "My Auto";
+class ServoBot: public frc::SampleRobot {
+
+	frc::Joystick stick { 0 };
 	frc::Servo servo {8};
 
 public:
-	Robot() {
+	ServoBot() {
 
 	}
-
-	 void RobotInit() {
-		chooser.AddDefault(autoNameDefault, autoNameDefault);
-		chooser.AddObject(autoNameCustom, autoNameCustom);
-		frc::SmartDashboard::PutData("Auto Modes", &chooser);
-	}
-
 
 
 	/*
@@ -47,7 +26,6 @@ public:
 	 */
 	void OperatorControl() override {
 		while (IsOperatorControl() && IsEnabled()) {
-			// drive with arcade style (use right stick)
 			double amt = (stick.GetRawAxis(3)+1)*0.5;
 			frc::SmartDashboard::PutNumber("amt: ",amt);
 			servo.Set(amt);
@@ -57,12 +35,6 @@ public:
 		}
 	}
 
-	/*
-	 * Runs during test mode
-	 */
-	void Test() override {
-
-	}
 };
 
-START_ROBOT_CLASS(Robot)
+START_ROBOT_CLASS(ServoBot)
